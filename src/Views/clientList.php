41b33a7f99d7ac5ea22340dd32d0fd32c65c3f3f
raquel2002x <palomofuentesraquel2002@gmail.com>
2022-12-01 12:20:42 +0100

    insert table

--- src/Views/clientList.php
+++ src/Views/clientList.php
@@ -3,16 +3,32 @@
 
     <body>
         <main>
-            <?php
-                foreach ($data["client"] as $client){
-                    echo "{$client->getClient()}";
-                    echo "{$client->getIssue()}";
-                    echo "{$client->getId()}";
-                    echo "{$client->getEmail()}";
-                    echo "{$client->getPhone()}";
-                    echo "{$client->getDateTime()}";
-                }
-            ?>
+            <!-- On tables -->
+            <table class="table-id">...</table>
+            <table class="table-client">...</table>
+            <table class="table-issue">...</table>
+            <table class="table-phone">...</table>
+            <table class="table-email">...</table>
+            <table class="table-dateTime">...</table>
+
+            <!-- On rows -->
+            <tr class="table-primary">...</tr>
+            <tr class="table-secondary">...</tr>
+            <tr class="table-success">...</tr>
+            <tr class="table-danger">...</tr>
+            <tr class="table-warning">...</tr>
+            <tr class="table-info">...</tr>
+
+
+            <!-- On cells (`td` or `th`) -->
+            <tr>
+            <td class="table-primary">...</td>
+            <td class="table-secondary">...</td>
+            <td class="table-success">...</td>
+            <td class="table-danger">...</td>
+            <td class="table-warning">...</td>
+            <td class="table-info">...</td>
+            </tr>
         </main>
     </body>
 
